78f89344dd542fb47c9386d053071e28d20d3629
ZekerZhayard <trees1000@qq.com>
2023-04-18 19:41:40 +0800

    1.0

---
+++ src/main/java/io/github/zekerzhayard/npe_dimensiontype/mixin/MixinDimensionType.java
@@ -0,0 +1,9 @@
+package io.github.zekerzhayard.npe_dimensiontype.mixin;
+
+import net.minecraft.world.dimension.DimensionType;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(DimensionType.class)
+public abstract class MixinDimensionType {
+
+}
